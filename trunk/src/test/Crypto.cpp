58ad522dabd469081cffeebace061e70234655b4
athipr <athipr@4cf9def3-4218-e229-0d58-58f9a8a57194>
2012-08-02 16:28:56 +0000

    add a new ECC because the previous one is extremely slow\!

--- trunk/src/test/Crypto.cpp
+++ trunk/src/test/Crypto.cpp
@@ -15,7 +15,11 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //=============================================================================
 
+#define TIME_TEST 0
+
+#if TIME_TEST
 #include <sys/time.h>
+#endif
 #include <stdio.h>
 #include <string>
 #include <utils/CryptoKey.h>
@@ -28,8 +32,11 @@ using namespace sai::utils;
 void testEncryptAES(int argc, char * argv[], std::string iv, std::string& output);
 void testDecryptAES(std::string input, std::string iv, std::string& output);
 
-void testEncryptECC(int argc, char * argv[], std::string& output, std::string& privKey);
-void testDecryptECC(std::string input, std::string& privKey, std::string& output);
+void testEncryptECC1(int argc, char * argv[], std::string& output, std::string& privKey);
+void testDecryptECC1(std::string input, std::string& privKey, std::string& output);
+
+void testEncryptECC2(int argc, char * argv[], std::string& output, std::string& privKey);
+void testDecryptECC2(std::string input, std::string& privKey, std::string& output);
 
 int main(int argc, char * argv[])
 {
@@ -56,21 +63,102 @@ int main(int argc, char * argv[])
   printf("--------------------------------------------\n");
 
   std::string priv;
-  testEncryptECC(argc, argv, out, priv);
-  testDecryptECC(out, priv, orig);
+  testEncryptECC1(argc, argv, out, priv);
+  testDecryptECC1(out, priv, orig);
 
+  printf("--------------------------------------------\n");
+
+  testEncryptECC2(argc, argv, out, priv);
+  testDecryptECC2(out, priv, orig);
   return 0;
 }
 
+#if TIME_TEST
 uint32_t diff(struct timeval tv1, struct timeval tv2)
 {
   uint64_t v1 = (tv1.tv_sec * 10000000000LL) + tv1.tv_usec;
   uint64_t v2 = (tv2.tv_sec * 10000000000LL) + tv2.tv_usec;
-  return (uint32_t)((v2 - v1) / 10000000);
+  return (uint32_t)((v2 - v1) / 10000);
+}
+#endif
+
+void 
+testEncryptECC1(int argc, char * argv[], std::string& output, std::string& privKey)
+{
+  std::string data = argv[1];
+
+  CryptoKeyFactory keyFactory;
+  CryptoFactory cryptFactory;
+
+  AsymmetricKey * akey = static_cast<AsymmetricKey*>(keyFactory.create(ECC521));
+  AsymmetricCrypto * acrypt = static_cast<AsymmetricCrypto*>(cryptFactory.create(ECC521));
+
+#if TIME_TEST
+  struct timeval tv1,tv2,tv3,tv4;
+#endif
+  printf("Generating Key....\n");
+#if TIME_TEST
+  gettimeofday(&tv1, 0);
+#endif
+  akey->generate();
+#if TIME_TEST
+  gettimeofday(&tv2, 0);
+#endif
+  printf("Done\n");
+  privKey = akey->getPrivateKeyString();
+
+#if TIME_TEST
+  gettimeofday(&tv3, 0);
+#endif
+  acrypt->encrypt(*akey, data, output);
+#if TIME_TEST
+  gettimeofday(&tv4, 0);
+#endif
+  printf("      Text: %s\n", data.c_str());
+#if PRINT_CIPHER
+  printf("CipherText: %s\n", output.c_str());
+#endif
+
+  delete akey;
+  delete acrypt;
+
+#if TIME_TEST
+  printf("Generate key %u usec\n", diff(tv1, tv2));
+  printf("Encrypt %u usec\n", diff(tv3, tv4));
+#endif
 }
 
 void 
-testEncryptECC(int argc, char * argv[], std::string& output, std::string& privKey)
+testDecryptECC1(std::string input, std::string& privKey, std::string& output)
+{
+  CryptoKeyFactory keyFactory;
+  CryptoFactory cryptFactory;
+
+  AsymmetricKey * akey = static_cast<AsymmetricKey*>(keyFactory.create(ECC521));
+  AsymmetricCrypto * acrypt = static_cast<AsymmetricCrypto*>(cryptFactory.create(ECC521));
+
+  akey->setPrivateKey(privKey);
+
+#if TIME_TEST
+  struct timeval tv1,tv2;
+  gettimeofday(&tv1, 0);
+#endif
+  acrypt->decrypt(*akey, input, output);
+#if TIME_TEST
+  gettimeofday(&tv2, 0);
+  printf("Decrypt %u usec\n", diff(tv1, tv2));
+#endif
+#if PRINT_CIPHER
+  printf("CipherText: %s\n", input.c_str());
+#endif
+  printf("      Text: %s\n", output.c_str());
+
+  delete akey;
+  delete acrypt;
+}
+
+void 
+testEncryptECC2(int argc, char * argv[], std::string& output, std::string& privKey)
 {
   std::string data = argv[1];
 
@@ -80,17 +168,27 @@ testEncryptECC(int argc, char * argv[], std::string& output, std::string& privKe
   AsymmetricKey * akey = static_cast<AsymmetricKey*>(keyFactory.create(ECC571));
   AsymmetricCrypto * acrypt = static_cast<AsymmetricCrypto*>(cryptFactory.create(ECC571));
 
+#if TIME_TEST
   struct timeval tv1,tv2,tv3,tv4;
+#endif
   printf("Generating Key....\n");
+#if TIME_TEST
   gettimeofday(&tv1, 0);
+#endif
   akey->generate();
+#if TIME_TEST
   gettimeofday(&tv2, 0);
+#endif
   printf("Done\n");
   privKey = akey->getPrivateKeyString();
 
+#if TIME_TEST
   gettimeofday(&tv3, 0);
+#endif
   acrypt->encrypt(*akey, data, output);
+#if TIME_TEST
   gettimeofday(&tv4, 0);
+#endif
   printf("      Text: %s\n", data.c_str());
 #if PRINT_CIPHER
   printf("CipherText: %s\n", output.c_str());
@@ -99,12 +197,14 @@ testEncryptECC(int argc, char * argv[], std::string& output, std::string& privKe
   delete akey;
   delete acrypt;
 
-  printf("Generate key %u msec\n", diff(tv1, tv2));
-  printf("Encrypt %u msec\n", diff(tv3, tv4));
+#if TIME_TEST
+  printf("Generate key %u usec\n", diff(tv1, tv2));
+  printf("Encrypt %u usec\n", diff(tv3, tv4));
+#endif
 }
 
 void 
-testDecryptECC(std::string input, std::string& privKey, std::string& output)
+testDecryptECC2(std::string input, std::string& privKey, std::string& output)
 {
   CryptoKeyFactory keyFactory;
   CryptoFactory cryptFactory;
@@ -114,11 +214,15 @@ testDecryptECC(std::string input, std::string& privKey, std::string& output)
 
   akey->setPrivateKey(privKey);
 
+#if TIME_TEST
   struct timeval tv1,tv2;
   gettimeofday(&tv1, 0);
+#endif
   acrypt->decrypt(*akey, input, output);
+#if TIME_TEST
   gettimeofday(&tv2, 0);
-  printf("Decrypt %u msec\n", diff(tv1, tv2));
+  printf("Decrypt %u usec\n", diff(tv1, tv2));
+#endif
 #if PRINT_CIPHER
   printf("CipherText: %s\n", input.c_str());
 #endif
@@ -140,11 +244,21 @@ testDecryptAES(std::string input, std::string iv, std::string& output)
   skey->setKey("HelloWorld");
   skey->setIV(iv);
 
+#if TIME_TEST
+  struct timeval tv1,tv2;
+  gettimeofday(&tv1, 0);
+#endif
   scrypt->decrypt(*skey, input, output);
+#if TIME_TEST
+  gettimeofday(&tv2, 0);
+#endif
 #if PRINT_CIPHER
   printf("CipherText: %s\n", input.c_str());
 #endif
   printf("      Text: %s\n", output.c_str());
+#if TIME_TEST
+  printf("Decrypt %u usec\n", diff(tv1, tv2));
+#endif
 
   delete skey;
   delete scrypt;
@@ -164,10 +278,20 @@ testEncryptAES(int argc, char * argv[], std::string iv, std::string& output)
   skey->setKey("HelloWorld");
   skey->setIV(iv);
 
+#if TIME_TEST
+  struct timeval tv1,tv2;
+  gettimeofday(&tv1, 0);
+#endif
   scrypt->encrypt(*skey, data, output);
+#if TIME_TEST
+  gettimeofday(&tv2, 0);
+#endif
   printf("      Text: %s\n", data.c_str());
 #if PRINT_CIPHER
   printf("CipherText: %s\n", output.c_str());
+#endif
+#if TIME_TEST
+  printf("Encrypt %u usec\n", diff(tv1, tv2));
 #endif
 
   delete skey;
